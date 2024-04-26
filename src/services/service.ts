e65b0dae01c8746f5106d5432029460d60c85dff
ImLauraGS <lgilsolano@gmail.com>
2024-04-26 20:10:19 +0200

    initial commit: typescript configuration, taildwind and fetch to api

---
+++ src/services/service.ts
@@ -0,0 +1,18 @@
+
+function dadJokesService() {
+  return {
+    getDadJoke: async () => {
+
+        const API_URL = 'https://icanhazdadjoke.com/';
+
+        const response = await fetch(API_URL, {
+            headers: {
+                Accept: 'application/json',
+            },
+        });
+        const data = await response.json();
+        return data.joke;
+
+    },
+  };
+}
